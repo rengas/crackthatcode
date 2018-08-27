7ba115375aff4c76a7a936394c7c304599338685
rengas@users.noreply.github.com <your-correct-email@example.com>
2018-08-27 10:48:47 -0700

    [C1Q4] Using String Contains method

--- src/com/crackthatcode/chapter1/Q4.java
+++ src/com/crackthatcode/chapter1/Q4.java
@@ -1,4 +1,41 @@
 package com.crackthatcode.chapter1;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+=
+/**
+ * Check a method anagram or not.
+ */
 public class Q4 {
+
+    public boolean stringContainsImpl(String str1, String str2){
+
+        boolean flag= false;
+        if(str1.length()==str2.length()&&str1.contains(str2)) {
+            flag=true;
+        }
+        return flag;
+    }
+
+    @Test
+    public void stringContainsImplTest(){
+        //positive case
+        String str1= "AAB";
+        String str2 = "BAA";
+
+        assertFalse(stringContainsImpl(str1,str2));
+
+        // Test for false
+        String str3= "Reverse";
+        String str4= "esreveL";
+        assertFalse(stringContainsImpl(str3,str4));
+
+        // Test for Length
+        String str5= "Reverse";
+        String str6= "esrevellR";
+        assertFalse(stringContainsImpl(str5,str6));
+
+    }
+
 }
